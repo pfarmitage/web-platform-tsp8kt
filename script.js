2530fbc2fdae75ee8afbf4a19edd244519d1dae3
pfarmitage <47189004+pfarmitage@users.noreply.github.com>
2023-03-18 23:35:03 +0000

    3 files modified, 1 added, 1 deleted

--- script.js
+++ script.js
@@ -15,435 +15,23 @@ document.getElementById("play-count").innerHTML = userData.playCount;
 document.getElementById("win-count").innerHTML = userData.winCount;
 document.getElementById("streak-count").innerHTML = userData.streak;
 
-// List of words associated with the theme
-//import { wordList,themeList } from './wordlist.js';
-const wordList = [
-  'apple',
-  'apricot',
-  'avocado',
-  'banana',
-  'blackberry',
-  'blueberry',
-  'boysenberry',
-  'cantaloupe',
-  'cherry',
-  'coconut',
-  'cranberry',
-  'date',
-  'dragonfruit',
-  'elderberry',
-  'fig',
-  'grape',
-  'grapefruit',
-  'guava',
-  'honeydew',
-  'kiwi',
-  'lemon',
-  'lime',
-  'lychee',
-  'mango',
-  'melon',
-  'orange',
-  'papaya',
-  'passionfruit',
-  'peach',
-  'pear',
-  'pineapple',
-  'plum',
-  'pomegranate',
-  'raspberry',
-  'strawberry',
-  'tangerine',
-  'watermelon',
-  'artichoke',
-  'asparagus',
-  'beet',
-  'broccoli',
-  'cabbage',
-  'carrot',
-  'cauliflower',
-  'celery',
-  'corn',
-  'cucumber',
-  'eggplant',
-  'garlic',
-  'ginger',
-  'kale',
-  'leek',
-  'lettuce',
-  'mushroom',
-  'onion',
-  'pepper',
-  'potato',
-  'pumpkin',
-  'radish',
-  'spinach',
-  'squash',
-  'swede',
-  'tomato',
-  'turnip',
-  'yam',
-  'zucchini',
-  'antelope',
-  'badger',
-  'bat',
-  'bear',
-  'beaver',
-  'buffalo',
-  'camel',
-  'cat',
-  'cheetah',
-  'chipmunk',
-  'cougar',
-  'cow',
-  'coyote',
-  'deer',
-  'dog',
-  'dolphin',
-  'donkey',
-  'elephant',
-  'elk',
-  'ferret',
-  'fox',
-  'gazelle',
-  'giraffe',
-  'goat',
-  'goose',
-  'gorilla',
-  'hamster',
-  'hare',
-  'hedgehog',
-  'hippopotamus',
-  'horse',
-  'hyena',
-  'jackal',
-  'jaguar',
-  'kangaroo',
-  'koala',
-  'leopard',
-  'lion',
-  'llama',
-  'lynx',
-  'meerkat',
-  'moose',
-  'mouse',
-  'otter',
-  'panda',
-  'panther',
-  'penguin',
-  'pig',
-  'porcupine',
-  'rabbit',
-  'raccoon',
-  'rat',
-  'rhinoceros',
-  'seal',
-  'sheep',
-  'skunk',
-  'sloth',
-  'snake',
-  'squirrel',
-  'tiger',
-  'walrus',
-  'weasel',
-  'whale',
-  'wolf',
-  'wombat',
-  'zebra',
-  'amsterdam',
-  'barcelona',
-  'berlin',
-  'boston',
-  'chicago',
-  'dublin',
-  'istanbul',
-  'london',
-  'madrid',
-  'miami',
-  'moscow',
-  'paris',
-  'rome',
-  'seattle',
-  'sydney',
-  'tokyo',
-  'toronto',
-  'basketball',
-  'baseball',
-  'bowling',
-  'cycling',
-  'football',
-  'golf',
-  'hiking',
-  'hockey',
-  'karate',
-  'running',
-  'skiing',
-  'swimming',
-  'tennis',
-  'volleyball',
-  'bed',
-  'chair',
-  'couch',
-  'desk',
-  'dresser',
-  'lamp',
-  'mirror',
-  'rug',
-  'table',
-  'bookcase',
-  'cabinet',
-  'chest',
-  'counter',
-  'futon',
-  'headboard',
-  'hutch',
-  'loveseat',
-  'nightstand',
-  'ottoman',
-  'recliner',
-  'sofa',
-  'stool',
-  'wardrobe',
-];
+const instructionsButton = document.querySelector('.header-icon');
+const instructionsModal = document.querySelector('#instructions-modal');
+const closeModalButton = document.querySelector('#close-modal');
 
-const themeList = [
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'fruit',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'vegetable',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'animal',
-  'city',
-  'city',
-  'city',
-  'city',
-  'city',
-  'city',
-  'city',
-  'city',
-  'city',
-  'city',
-  'city',
-  'city',
-  'city',
-  'city',
-  'city',
-  'city',
-  'city',
-  'city',
-  'sport',
-  'sport',
-  'sport',
-  'sport',
-  'sport',
-  'sport',
-  'sport',
-  'sport',
-  'sport',
-  'sport',
-  'sport',
-  'sport',
-  'sport',
-  'sport',
-  'furniture',
-  'furniture',
-  'furniture',
-  'furniture',
-  'furniture',
-  'furniture',
-  'furniture',
-  'furniture',
-  'furniture',
-  'furniture',
-  'furniture',
-  'furniture',
-  'furniture',
-  'furniture',
-  'furniture',
-  'furniture',
-  'furniture',
-  'furniture',
-  'furniture',
-  'furniture',
-  'furniture',
-  'furniture',
-  'furniture',
-];
-const wordObjects = [
-  { word: 'amber', theme: 'colors' },
-  { word: 'beach', theme: 'nature' },
-  { word: 'bison', theme: 'animals' },
-  { word: 'bloom', theme: 'nature' },
-  { word: 'cabin', theme: 'buildings' },
-  { word: 'cactus', theme: 'plants' },
-  { word: 'canoe', theme: 'vehicles' },
-  { word: 'cedar', theme: 'plants' },
-  { word: 'chirp', theme: 'sounds' },
-  { word: 'cliff', theme: 'nature' },
-  { word: 'comet', theme: 'space' },
-  { word: 'coral', theme: 'ocean' },
-  { word: 'crisp', theme: 'food' },
-  { word: 'crowd', theme: 'people' },
-  { word: 'daisy', theme: 'plants' },
-  { word: 'dough', theme: 'food' },
-  { word: 'drift', theme: 'nature' },
-  { word: 'eagle', theme: 'animals' },
-  { word: 'emery', theme: 'materials' },
-  { word: 'flame', theme: 'fire' },
-  { word: 'frost', theme: 'weather' },
-  { word: 'glaze', theme: 'food' },
-  { word: 'gloom', theme: 'feelings' },
-  { word: 'grape', theme: 'fruit' },
-  { word: 'hazel', theme: 'colors' },
-  { word: 'honey', theme: 'food' },
-  { word: 'hound', theme: 'animals' },
-  { word: 'jolly', theme: 'feelings' },
-  { word: 'kayak', theme: 'vehicles' },
-  { word: 'laser', theme: 'technology' },
-  { word: 'lemon', theme: 'fruit' },
-  { word: 'lunar', theme: 'space' },
-  { word: 'maple', theme: 'plants' },
-  { word: 'melon', theme: 'fruit' },
-  { word: 'oasis', theme: 'nature' },
-  { word: 'olive', theme: 'fruit' },
-  { word: 'opera', theme: 'music' },
-  { word: 'peach', theme: 'fruit' },
-  { word: 'pearl', theme: 'materials' },
-  { word: 'petal', theme: 'plants' },
-  { word: 'quilt', theme: 'materials' },
-  { word: 'raven', theme: 'animals' },
-  { word: 'sable', theme: 'animals' },
-  { word: 'scoop', theme: 'utensils' },
-  { word: 'skate', theme: 'vehicles' },
-  { word: 'snack', theme: 'food' },
-  { word: 'spice', theme: 'food' },
-  { word: 'swirl', theme: 'shapes' },
-];
+instructionsButton.addEventListener('click', () => {
+  instructionsModal.style.display = 'block';
+});
+
+closeModalButton.addEventListener('click', () => {
+  instructionsModal.style.display = 'none';
+});
+
+
+// List of words associated with the theme
+var wordObjects = window.wordObjectsList;
+console.log(wordObjects);
+
 
 //Pick three words
 let words = [];
@@ -513,6 +101,7 @@ for (let i = 0; i < words.length; i++) {
     }
   }
 }
+console.log(letters + originalLetters);
 
 //add target score to page
 //document.getElementById('targetScore').innerHTML = 'Target: ' + targetScore;
@@ -558,7 +147,6 @@ function deleteLetter(letter) {
   inputField.value = inputField.value.slice(0, -1);
 }
 
-
 // Function to submit a word and update the score
 let score = 0;
 let percent = 0;
@@ -575,7 +163,10 @@ function startTimer() {
   startTime = new Date().getTime();
   timerInterval = setInterval(function () {
     seconds++;
-    document.getElementById('timer').innerHTML = seconds + ' seconds';
+    var timer = document.getElementById('timer');
+    var animation = ['-', '\\', '|', '/'];
+    var index = seconds % animation.length;
+    timer.innerHTML = seconds + ' seconds ' + animation[index];
   }, 1000);
   //increment playCount
   userData.playCount++;
@@ -586,7 +177,6 @@ function startTimer() {
   document.getElementById("streak-count").innerHTML = userData.streak;
   document.getElementById('game-board').style.display = 'flex';
   document.getElementById('start-button').style.display = 'none';
-
 }
 
 function stopTimer() {
@@ -606,18 +196,17 @@ function submitWord() {
     }
     score += wordScore;
     percent = parseInt((score / targetScore) * 100);
-    let wordList = document.getElementById('word-list');
+    let submittedList = document.getElementById('submitted-list');
     let listItem = document.createElement('li');
     listItem.innerHTML = word + '(' + wordScore + ')';
-    wordList.appendChild(listItem);
+    submittedList.appendChild(listItem);
     updateLetterCounts(word);
     wordInput.value = '';
     if (percent >= 100) {
       stopTimer();
-      document.getElementById('score').innerHTML =
-        'You Win! Time: ' + elapsedTime + 's';
+      //document.getElementById('score').innerHTML = 'You Win! Time: ' + elapsedTime + 's';
       clearInterval(timerInterval);
-      //increment winCount
+      //increment winCount and streak
       userData.winCount++;
       userData.streak++;
       localStorage.setItem("userData", JSON.stringify(userData));
@@ -626,11 +215,9 @@ function submitWord() {
       document.getElementById('submit-form').style.display = 'none';
       document.getElementById('win-tick').style.display = 'flex';
       document.getElementById('crosses').style.display = 'none';
-
       alert("You Win! Time: " + elapsedTime + "s");
     } else {
-      document.getElementById('score').innerHTML =
-        'Score: ' + score + ' (' + percent + '%)';
+      //document.getElementById('score').innerHTML = 'Score: ' + score + ' (' + percent + '%)';
     }
   } else {
     //alert('Not enough letters or not in word list!');
@@ -645,7 +232,7 @@ function isValidWord(word) {
       alert('Not enough letters');
       return false;
     }
-    letters[letter]--; // Subtract one from the available count for each letter used
+    //letters[letter]--; // Subtract one from the available count for each letter used
   }
   // Check if the word is in the wordObjects list
   let lowerCaseWord = word.toLowerCase();
@@ -668,7 +255,9 @@ function isValidWord(word) {
 function updateLetterCounts(word) {
   for (let i = 0; i < word.length; i++) {
     let letter = word[i];
+    console.log(letters);
     letters[letter]--;
+    console.log(letters[letter] + '-'+letter)
     let countLabel = document
       .getElementById('tile-' + letter)
       .getElementsByClassName('count-label')[0];
@@ -687,8 +276,8 @@ function resetGame() {
   percent = 0;
   submittedWords = [];
   // Update the UI to reflect the reset values
-  document.getElementById('score').innerHTML = 'Score: 0 (0%)';
-  document.getElementById('word-list').innerHTML = '';
+  //document.getElementById('score').innerHTML = 'Score: 0 (0%)';
+  document.getElementById('submitted-list').innerHTML = '';
   resetLetterCounts();
 }
 
